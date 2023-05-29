13650755f3baecaa2b4e654cf9210b9d711e9b8b
KalilRamos <134665832+KalilRamos@users.noreply.github.com>
2023-05-29 08:23:15 -0300

    protótipo 1.1
    Esse protótipo tem as funções de adicionar os produtos e seus valores e listar todos os produtos com um menu.

---
+++ trabalho ES 2.py
@@ -0,0 +1,33 @@
+produtos = {}  # Dicionário para armazenar os produtos e seus valores
+
+def adicionar_produto():
+    nome = input('Qual o nome do produto?')
+    valor = float(input('Ok! Mas qual é o valor do produto?'))
+    produtos[nome] = valor
+
+def listar_produtos():
+    if produtos:
+        print('Esses são os produtos cadastrados atualmente:')
+        for nome, valor in produtos.items():
+            print(f'{nome}: R${valor:.2f}')
+    
+    else:
+        print('Você ainda não cadastrou nenhum produto')
+
+while True:
+    print('\n---- Menu ----')
+    print('1. Cadastro de Produtos')
+    print('2. Tabela de Produtos')
+    print('3. sair')
+
+    acao = input('Escolha uma das opções do menu.')
+
+    if acao == '1':
+        adicionar_produto()
+    elif acao == '2':
+        listar_produtos()
+    elif acao == '3':
+        print('Saindo...')
+        break
+    else:
+        print('Por favor, escolha uma das opções dentre as listadas no menu.')
