ac840815aa656361f87b160540c65918112675a0
KalilRamos <kalilramos6@gmail.com>
2023-06-11 18:08:51 -0300

    refatoracao
    remoção de arquivos não utilizados e adição do main

--- v2/listarProdutos.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from db.models.Product import Product
-
-def listarProdutos(db: Session):
-    produtos = db.query(Product).all()
-    return {"Produtos": produtos}
