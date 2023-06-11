835f3161e7703ae898c51cbc82f1fa447e26affa
KalilRamos <kalilramos6@gmail.com>
2023-06-11 18:15:35 -0300

    Update database.py

--- v2/db/database.py
+++ v2/db/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import URL, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+#Alterar para a suas credenciais do banco de dados
 DB_URL = URL.create(
-    "mysql",
+    "mysql", 
     username="root",
     password="root",
     host="localhost",
